d12647463316e9973064688be8f0e883bb952966
M477 <m.477h3w@live.co.uk>
2013-10-03 15:10:32 +0100

    Removed hard logging option for the moment and did minor edits

---
+++ web/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+DEFINE( "DATA_HOST",   "localhost" );
+DEFINE( "DATA_PORT",   "3306" );
+DEFINE( "DATA_USER",   "db_username" );
+DEFINE( "DATA_PASS",   "db_password" );
+DEFINE( "DATA_BASE",   "cynichat" );
+DEFINE( "DATA_PREFIX", "cyni_" );
+
+DEFINE( "NUM_CONST",   26 );
+DEFINE( "HOME_DIR",    "/var/www/chatter/" );
+DEFINE( "HOME_DNS",    "127.0.0.1" );
+DEFINE( "HOME_URL",    "/chatter/" );
+
+?>
