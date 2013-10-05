adb0db70d4e8b92e90bbc35e7988e4056b81c9b4
M477 <m.477h3w@live.co.uk>
2013-10-05 23:56:06 +0100

    Probably going to be the last commit for the WebUI for a while... I am now sick of looking at PHP and that kinda has an impact...

--- web/index.php
+++ web/index.php
@@ -1,8 +1,12 @@
 <?php
 
-require_once '/var/www/chatter/config/config.php';
+require_once 'config/config.php';
 require_once HOME_DIR."bases/cyniBase.php";
 require_once HOME_DIR."funcs/template.php";
+
+if ( file_exists( "install/" ) ) {
+	header( "Location: install/create.php" );
+}
 
 $base = new cyniBase();
 $pg = new pageTemplate( "index.html" );
