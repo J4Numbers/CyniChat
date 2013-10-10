17dd60adf63c9140aeec3559515ccf17158cf1af
James <beardyjames@live.co.uk>
2013-10-10 11:51:35 +0100

    Ambiguous Protocol spec is ambiguous.

--- src/main/java/uk/co/CyniCode/CyniChat/bungee/BungeeChannelProxy.java
+++ src/main/java/uk/co/CyniCode/CyniChat/bungee/BungeeChannelProxy.java
@@ -5,9 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
@@ -21,104 +18,108 @@ import uk.co.CyniCode.CyniChat.routing.IChatEndpoint;
 
 public class BungeeChannelProxy implements PluginMessageListener, IChatEndpoint {
 
-	public CyniChat plugin;
+    public CyniChat plugin;
 
-	public BungeeChannelProxy(CyniChat plugin) {
-		plugin.getServer().getMessenger().registerOutgoingPluginChannel(plugin, "BungeeCord");
-		this.plugin = plugin;
-		plugin.getServer().getMessenger().registerIncomingPluginChannel(plugin, "CyniChat", this);
-	}
+    public BungeeChannelProxy(CyniChat plugin) {
+        plugin.getServer().getMessenger().registerOutgoingPluginChannel(plugin, "BungeeCord");
+        this.plugin = plugin;
+        plugin.getServer().getMessenger().registerIncomingPluginChannel(plugin, "BungeeCord", this);
+    }
 
-	public void onPluginMessageReceived(String pluginChnl, Player plr, byte[] data) {
-		try {
-			
-			CyniChat.printDebug( "Channel recieved : " + pluginChnl );
-			
-			if (!pluginChnl.equals("CyniChat")) {
-				CyniChat.printWarning("BungeeChannelProxy was given message for channel " + pluginChnl);
-				return;
-			}
+    public void onPluginMessageReceived(String pluginChnl, Player plr, byte[] data) {
+        try {
 
-			CyniChat.printDebug( "CyniChat message recieved!" );
-			
-			DataInputStream dis = new DataInputStream(new ByteArrayInputStream(data));
+            CyniChat.printDebug("Channel recieved : " + pluginChnl);
 
-			//TODO - Channel protection
-			EndpointType type = EndpointType.values()[dis.readInt()];
-			String fancyPlayerName = dis.readUTF();
-			String playerName = dis.readUTF();
-			String chatChannel = dis.readUTF();
-			String message = dis.readUTF();
-			
-			//if this is a player type message, use the fancy name, else use the regular name
-			if (DataManager.getChannel(chatChannel) != null) {
-				if(type == EndpointType.PLAYER){
-					ChatRouter.routeMessage(EndpointType.BUNGEE, fancyPlayerName, chatChannel, message);
-				}
-				else
-				{
-					ChatRouter.routeMessage(type, playerName, chatChannel, message);
-				}
-				
-			}
-		} catch (IOException ex) {
-			CyniChat.printSevere("Error parsing message in BungeeChannelProxy");
-			ex.printStackTrace();
-		}
-	}
+            if (!pluginChnl.equals("BungeeCord")) {
+                CyniChat.printWarning("BungeeChannelProxy was given message for channel " + pluginChnl);
+                return;
+            }
 
-	public void giveMessage(EndpointType type, String player, String channel, String message) {
-		try {
-			//Create message
-			ByteArrayOutputStream b = new ByteArrayOutputStream();
-			DataOutputStream out = new DataOutputStream(b);
-			out.writeInt(type.ordinal());// typeId
-			//Fancy name transmission
-			if(type == EndpointType.PLAYER){
-				
-				try {
-					CyniChat.printDebug( player );
-					CyniChat.printDebug( channel );
-					CyniChat.printDebug( message );
-					Player thisOne = Bukkit.getPlayerExact(player);
-					
-					if ( thisOne != null ) {
-						CyniChat.printDebug( "Player recognised, name of : " + thisOne.getDisplayName() );
-					} else {
-						CyniChat.printDebug( "Null found" );
-					}
-					
-					out.writeUTF(PermissionManager.getPlayerFull(thisOne));
-				} catch ( NullPointerException e ) {
-					CyniChat.printDebug( "Null error..." );
-					e.printStackTrace();
-					return;
-				}
-				
-			}
-			else
-			{
-				out.writeUTF("");
-			}
-			out.writeUTF(player);
-			out.writeUTF(channel);
-			out.writeUTF(message);
+            DataInputStream in = new DataInputStream(new ByteArrayInputStream(data));
+            String subChannel = in.readUTF();
+            if(!subChannel.equals("CyniChat")){return;/*Not our problem*/}
+            
+            short len = in.readShort();
+            byte[] msgbytes = new byte[len];
+            in.readFully(msgbytes);
 
-			ByteArrayOutputStream msgBytes = new ByteArrayOutputStream();
-			DataOutputStream msg = new DataOutputStream(msgBytes);
-			msg.writeUTF("Forward");
-			msg.writeUTF("ALL");
-			msg.writeUTF("CyniChat");
-			//Push message content
-			msg.writeShort(b.toByteArray().length);
-			msg.write(b.toByteArray());
-			
-			Player p = Bukkit.getOnlinePlayers()[0];
-			p.sendPluginMessage(plugin, "BungeeCord", msgBytes.toByteArray());
-			CyniChat.printDebug( "Message sent!" );
-		} catch (IOException ex) {
-			CyniChat.printSevere("Error sending message to BungeeChannelProxy");
-			ex.printStackTrace();;
-		}
-	}
+            CyniChat.printDebug("CyniChat message recieved!");
+
+            
+            DataInputStream dis = new DataInputStream(new ByteArrayInputStream(msgbytes));
+            
+            EndpointType type = EndpointType.values()[dis.readInt()];
+            String fancyPlayerName = dis.readUTF();
+            String playerName = dis.readUTF();
+            String chatChannel = dis.readUTF();
+            String message = dis.readUTF();
+
+            //if this is a player type message, use the fancy name, else use the regular name
+            if (DataManager.getChannel(chatChannel) != null) {
+                if (type == EndpointType.PLAYER) {
+                    ChatRouter.routeMessage(EndpointType.BUNGEE, fancyPlayerName, chatChannel, message);
+                } else {
+                    ChatRouter.routeMessage(type, playerName, chatChannel, message);
+                }
+
+            }
+        } catch (IOException ex) {
+            CyniChat.printSevere("Error parsing message in BungeeChannelProxy");
+            ex.printStackTrace();
+        }
+    }
+
+    public void giveMessage(EndpointType type, String player, String channel, String message) {
+        try {
+            //Create message
+            ByteArrayOutputStream b = new ByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(b);
+            out.writeInt(type.ordinal());// typeId
+            //Fancy name transmission
+            if (type == EndpointType.PLAYER) {
+
+                try {
+                    CyniChat.printDebug(player);
+                    CyniChat.printDebug(channel);
+                    CyniChat.printDebug(message);
+                    Player thisOne = Bukkit.getPlayerExact(player);
+
+                    if (thisOne != null) {
+                        CyniChat.printDebug("Player recognised, name of : " + thisOne.getDisplayName());
+                    } else {
+                        CyniChat.printDebug("Null found");
+                    }
+
+                    out.writeUTF(PermissionManager.getPlayerFull(thisOne));
+                } catch (NullPointerException e) {
+                    CyniChat.printDebug("Null error...");
+                    e.printStackTrace();
+                    return;
+                }
+
+            } else {
+                out.writeUTF("");
+            }
+            out.writeUTF(player);
+            out.writeUTF(channel);
+            out.writeUTF(message);
+
+            ByteArrayOutputStream msgBytes = new ByteArrayOutputStream();
+            DataOutputStream msg = new DataOutputStream(msgBytes);
+            msg.writeUTF("Forward");
+            msg.writeUTF("ALL");
+            msg.writeUTF("CyniChat");
+            //Push message content
+            msg.writeShort(b.toByteArray().length);
+            msg.write(b.toByteArray());
+
+            Player p = Bukkit.getOnlinePlayers()[0];
+            p.sendPluginMessage(plugin, "BungeeCord", msgBytes.toByteArray());
+            CyniChat.printDebug("Message sent!");
+        } catch (IOException ex) {
+            CyniChat.printSevere("Error sending message to BungeeChannelProxy");
+            ex.printStackTrace();
+        }
+    }
 }
