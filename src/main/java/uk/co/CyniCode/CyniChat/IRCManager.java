7caf7fbeffa24d2ddfb98bfc4735d11ab167be1e
J4Numbers <j4numbers@gmail.com>
2013-12-16 04:55:54 +0000

    Commented through all the irc classes

--- src/main/java/uk/co/CyniCode/CyniChat/IRCManager.java
+++ src/main/java/uk/co/CyniCode/CyniChat/IRCManager.java
@@ -2,9 +2,7 @@ package uk.co.CyniCode.CyniChat;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import uk.co.CyniCode.CyniChat.objects.UserDetails;
 import uk.co.CyniCode.CyniChat.routing.ChatRouter;
@@ -18,105 +16,189 @@ import org.pircbotx.exception.NickAlreadyInUseException;
 
 /**
  * An instantiation of a PircBotX bot
- *
+ * 
  * @author CyniCode
  */
 public class IRCManager implements IChatEndpoint {
 	
+	/**
+	 * This is the bot that is going to be doing all the legword
+	 */
 	private PircBotX bot;
+	
+	
+	
+	/**
+	 * This is a channel that has absolute control over 
+	 * the bot. People in this channel can restart, kill
+	 * and talk to the bot.
+	 */
+	private String ircMasterChannel;
+	
+	/**
+	 * This goes with the previous field, allowing connection
+	 * to a protected channel in IRC.
+	 */
+	private String ircMasterPassword;
 	
 	/**
 	 * Constructor for making a new Bot out of barely anything
-	 *
+	 * 
 	 * @param plugin : Used for getting the config options
 	 * @throws Exception : So much that could go wrong here...
 	 */
 	public IRCManager(CyniChat plugin) throws Exception {
 		
+		//Instantiate the bot
 		this.bot = new PircBotX();
 		
+		//And instantiate the master channel fields
+		this.ircMasterChannel = plugin.getConfig().getString("CyniChat.irc.admin_channel");
+		this.ircMasterPassword = plugin.getConfig().getString("CyniChat.irc.admin_password");
+		
+		//Then add the listener
 		this.bot.getListenerManager().addListener(new IrcChatListener());
 		
+		//And set the names of the bot
 		this.bot.setName(plugin.getConfig().getString("CyniChat.irc.nickname"));
 		this.bot.setLogin("CyniBot");
+		
 		try {
-			this.bot.connect(plugin.getConfig().getString("CyniChat.irc.hostname"), plugin.getConfig().getInt("CyniChat.irc.port"));
+			
+			//Then connect to the server in the config
+			this.bot.connect(plugin.getConfig().getString("CyniChat.irc.hostname"),
+					plugin.getConfig().getInt("CyniChat.irc.port"));
+			
+			//And the master channel with it...
+			this.bot.joinChannel( this.ircMasterChannel, this.ircMasterPassword );
+			
 		} catch (Exception e) {
+			
+			//Or try to in any case...
 			throw e;
+			
 		}
 		
 	}
 	
 	/**
 	 * Send a message through the bot in the format of : [SENDER] : [MESSAGE]
-	 *
+	 * 
 	 * @param chan : The channel we're sending the message on
 	 * @param sender : The sender who, you guessed it, sent the message
 	 * @param message : The message itself
 	 */
 	public void sendMessage(String chan, String sender, String message) {
-		if ( CyniChat.IRC == true && !"".equals( chan ) ) {
+		
+		//If the IRC is enabled and the channel is there
+		if ( CyniChat.IRC == true && !chan.equals( "" ) ) {
+			
+			//Get the channel that we're sending out over
 			Channel sendChan = this.bot.getChannel(chan);
+			
+			//Then send the message out over it
 			this.bot.sendMessage(sendChan, sender + " : " + message);
+			
 		}
+		
 	}
 	
 	/**
 	 * Send an action through the bot as a message in the form of [SENDER]
 	 * [ACTION]
-	 *
+	 * 
 	 * @param chan : The channel that we're transmitting stuff over
 	 * @param sender : The sender who sent the thing that was to be delivered
 	 * @param message : The action itself
 	 */
 	public void sendAction(String chan, UserDetails sender, String message) {
-		if ( CyniChat.IRC == true && !"".equals( chan ) ) {
+		
+		//Ask if IRC has been enabled and whether the channel
+		// is even there
+		if ( CyniChat.IRC == true && !chan.equals( "" ) ) {
+			
+			//Set the channel we're going to be sending over
 			Channel sendChan = this.bot.getChannel(chan);
+			
+			//Then send over that channel
 			this.bot.sendMessage(sendChan, message);
+			
 		}
+		
 	}
 	
 	/**
 	 * Load all possible channels into the plugin and see which of them are
 	 * actually going to be connecting to the IRC server and whatnot
-	 *
+	 * 
 	 * @param allChans : All the potential channels there are
 	 */
 	public void loadChannels(
 			Map<String, uk.co.CyniCode.CyniChat.objects.Channel> allChans) {
 		
+		//Initialise a new map for all the channels that are linked
+		// to IRC
 		Map<String, String> actingChans = new HashMap<String, String>();
 		
-		Set<String> keys = allChans.keySet();
-		Iterator<String> iter = keys.iterator();
-		
 		try {
+			
+			//Join the master channel
+			bot.joinChannel( getIrcMasterChannel(), getIrcMasterPassword() );
+			
+			//Then, for all of them...
 			for ( Map.Entry<String, uk.co.CyniCode.CyniChat.objects.Channel> thisChan
 					: allChans.entrySet() ) {
 				
+				//If the IRC channel does exist...
 				if ( !thisChan.getValue().getIRC().equals( "" ) ) {
-					bot.joinChannel(thisChan.getValue().getIRC().toLowerCase(), thisChan.getValue().getIRCPass());
-					actingChans.put(thisChan.getValue().getIRC().toLowerCase(), thisChan.getKey() );
+					
+					//Join the channel with the bot
+					bot.joinChannel(thisChan.getValue().getIRC().toLowerCase(),
+							thisChan.getValue().getIRCPass());
+					
+					//And put the channel into the map we prepared earlier
+					actingChans.put(thisChan.getValue().getIRC().toLowerCase(),
+							thisChan.getKey() );
+					
 				}
+				
 			}
 			
+			//Then set the map
 			CyniChat.data.setLinkedChans(actingChans);
+			
 		} catch (Exception e) {
 		}
+		
 	}
 	
 	/**
 	 * Restart the IRC component of the plugin via various means
 	 */
 	public void restart() {
+		
+		//Tell people that things are happening
 		CyniChat.printWarning("Restarting the IRC bot...");
 		CyniChat.printInfo("Stopping the IRC bot...");
+		
+		//And shut the bot down
 		bot.shutdown();
+		
+		//Then announce that we're going to start it up again
 		CyniChat.printInfo("Starting up the IRC bot again...");
+		
 		try {
+			
+			//And reconnect.
 			bot.reconnect();
+			
+			//Then load in all the channels again
 			loadChannels(CyniChat.data.getChannels());
+			
+			//And say that we were successful!
 			CyniChat.printInfo("Reconnected successfully");
+			
+		//Or fail miserably...
 		} catch (IOException e) {
 			CyniChat.printSevere("We could not connect...");
 			e.printStackTrace();
@@ -127,6 +209,18 @@ public class IRCManager implements IChatEndpoint {
 			CyniChat.printSevere("IRC has failed... call in the drones...");
 			e.printStackTrace();
 		}
+		
+	}
+	
+	/**
+	 * Given the name of a channel... get the channel object 
+	 *  associated with it
+	 * 
+	 * @param chanName : The channel name we're searching for
+	 * @return the Channel that belongs to that name
+	 */
+	public Channel getChannel( String chanName ) {
+		return bot.getChannel( chanName );
 	}
 	
 	/**
@@ -134,15 +228,71 @@ public class IRCManager implements IChatEndpoint {
 	 * knock it over the head with a bat.
 	 */
 	public void stop() {
+		
+		//Tell the console that stuff is happening
 		CyniChat.printInfo("Shutting down IRC...");
-		this.bot.shutdown(true);
+		
+		//And kill the bot with no chance of reconnection
+		bot.shutdown(true);
+		
+		//Then tell the people that the murder happened
 		CyniChat.printInfo("IRC has been killed.");
+		
 	}
 	
-	public void giveMessage(ChatRouter.EndpointType type, String player, String channel, String message) {
-		if(type == ChatRouter.EndpointType.PLAYER){
+	/**
+	 * As per interfacing...
+	 * 
+	 * @param type : The endpoint of the message
+	 * @param player : The player that sent the message
+	 * @param channel : The channel that the message was sent on
+	 * @param message : The message contents themselves
+	 */
+	public void giveMessage(ChatRouter.EndpointType type, String player,
+			String channel, String message) {
+		
+		//Is the endpoint a player?
+		if ( type == ChatRouter.EndpointType.PLAYER ) {
+			
+			//If so, get the channel from the package
 			String ircChannel = CyniChat.data.getChannel(channel).getIRC();
+			
+			//And send a message out over that channel
 			sendMessage(ircChannel, player, message);
+			
 		}
+		
+	}
+	
+	/**
+	 * Get the master channel name from the data.
+	 * @return the ircMasterChannel
+	 */
+	public String getIrcMasterChannel() {
+		return ircMasterChannel;
+	}
+	
+	/**
+	 * Get the password associated with the ircMasterChannel.
+	 * @return the ircMasterPassword
+	 */
+	public String getIrcMasterPassword() {
+		return ircMasterPassword;
+	}
+	
+	/**
+	 * Set the Master Channel to something else.
+	 * @param ircMasterChannel : The ircMasterChannel to set
+	 */
+	public void setIrcMasterChannel( String ircMasterChannel ) {
+		this.ircMasterChannel = ircMasterChannel;
+	}
+	
+	/**
+	 * Set the Master Channel's password to something else.
+	 * @param ircMasterPassword : The ircMasterPassword to set
+	 */
+	public void setIrcMasterPassword( String ircMasterPassword ) {
+		this.ircMasterPassword = ircMasterPassword;
 	}
 }
--- src/main/java/uk/co/CyniCode/CyniChat/irc/IrcChatListener.java
+++ src/main/java/uk/co/CyniCode/CyniChat/irc/IrcChatListener.java
@@ -1,7 +1,7 @@
 package uk.co.CyniCode.CyniChat.irc;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+//import org.bukkit.Bukkit;
+//import org.bukkit.entity.Player;
 
 import uk.co.CyniCode.CyniChat.routing.ChatRouter;
 import uk.co.CyniCode.CyniChat.CyniChat;
@@ -18,161 +18,381 @@ import org.pircbotx.hooks.events.PrivateMessageEvent;
 public class IrcChatListener extends ListenerAdapter {
 	
 	/**
-	 * Listen for all the chatter that is going on on the IRC bot's end so that
-	 * any commands given there are going to be executed while commands given
-	 * from inside MC will be left alone.
+	 * Listen for all the chatter that is going on on the
+	 * IRC bot's end so that any commands given there are
+	 * going to be executed while commands given from in 
+	 * MC will be left alone.
+	 * 
+	 * @param event : The event we're listening to
+	 * @throws Exception when something goes wrong
 	 */
 	public void onMessage(MessageEvent event) throws Exception {
+		
+		//Well we've been given a message. Is it a CyniBot
+		// command that we're hearing?
 		if (event.getMessage().startsWith(":?")) {
+			
+			//Yes. Let's split it into pieces
 			String[] argments = event.getMessage().split(" ");
+			
+			//And get what channel it is
 			org.pircbotx.Channel thisChan = event.getChannel();
 			
+			//Now... it could just be a testing ask
+			// for all the things the bot can do
 			if (argments.length == 1) {
+				
+				//In which case, send them the help
 				CyniChat.printDebug("Default used...");
 				IrcResponses.helpOutput(event.getBot(), event.getUser());
 				return;
+				
 			}
 			
+			//Or it could be a request for help, plain 
+			// and simple
 			if (argments[1].equalsIgnoreCase("help")) {
+				
+				//So we give it to them
 				CyniChat.printDebug("Help selected...");
 				IrcResponses.helpOutput(event.getBot(), event.getUser());
 				return;
+				
 			}
 			
+			//They could be asking for a list of players
+			// that are around...
 			if (argments[1].equalsIgnoreCase("list")) {
+				
 				CyniChat.printDebug("Listing chosen...");
+				
+				//And it's either asking for how many people
+				// are on the whole server
 				if (argments.length > 2) {
+					
+					//Well, we're going to have been given /something/
+					// as an additional argument
 					if (argments[2].equalsIgnoreCase("all")) {
+						
+						//And then we print out all the people
+						// that are on the server
 						CyniChat.printDebug("You've either got 'all' as parameter...");
 						CyniChat.printDebug(event.getUser().getNick() + " : " + thisChan.getName());
-						IrcResponses.listOutput(event.getUser(), event.getBot(), thisChan.getName(), true);
+						IrcResponses.listOutput(event.getUser(), event.getBot(),
+								thisChan.getName(), true);
 						return;
+						
 					}
+				
+				//Or it's asking for how many people are in this 
+				// specific channel
 				} else {
+					
+					//Or we get a list of all those online that are
+					// inside this channel, then print that out.
 					CyniChat.printDebug("Or you don't....");
 					CyniChat.printDebug(event.getUser().getNick() + " : " + thisChan.getName());
-					IrcResponses.listOutput(event.getUser(), event.getBot(), thisChan.getName(), false);
+					IrcResponses.listOutput(event.getUser(), event.getBot(),
+							thisChan.getName(), false);
 					return;
+					
 				}
+				
 				return;
+				
 			}
 			
+			//Well, now we get into administrative commands. 
+			// For instance, a person might need to be kicked
 			if (argments[1].equalsIgnoreCase("kick")
+					
+					//So we ask if they're an op in the channel
+					// that we're trying to kick someone in.
 					&& thisChan.isOp(event.getUser())) {
+				
 				CyniChat.printDebug("Kicking...");
+				
+				//And ask if they are actually trying to kick
+				// anyone.
 				if (argments[2] != null) {
-					IrcResponses.kickOutput(event.getUser(), event.getBot(), argments[2], thisChan.getName());
+					
+					//Then kick them.
+					IrcResponses.kickOutput(event.getUser(), event.getBot(),
+							argments[2], thisChan.getName());
 					return;
+					
 				}
+				
+				//Else we tell them that they need to kick /someone/
 				event.respond("I'm sorry, you must include a person to be kicked");
 				return;
+				
 			}
 			
+			//It could be a request for a ban/unban?
 			if ((argments[1].equalsIgnoreCase("ban")
 					|| argments[1].equalsIgnoreCase("unban"))
+					
+					//And once again, we need to make sure that they have
+					// some sort of authority to authorize such a move.
 					&& thisChan.isOp(event.getUser())) {
+				
 				CyniChat.printDebug("Banning...");
+				
+				//Are they even providing someone to un/ban
 				if (argments[2] != null) {
 					
+					//Then we define whether it's a ban...
 					if (argments[1].equalsIgnoreCase("ban")) {
-						IrcResponses.banOutput(event.getUser(), event.getBot(), argments[2], event.getChannel().getName(), false);
+						
+						//And if it's a ban, we try to ban the
+						// player
+						IrcResponses.banOutput(event.getUser(), event.getBot(),
+								argments[2], event.getChannel().getName(), false);
+						
+					//Or an unban...
 					} else {
-						IrcResponses.banOutput(event.getUser(), event.getBot(), argments[2], event.getChannel().getName(), true);
+						
+						//And since it's an unban, we try to unban
+						// this player
+						IrcResponses.banOutput(event.getUser(), event.getBot(),
+								argments[2], event.getChannel().getName(), true);
+						
 					}
+					
 					return;
+					
 				}
+				
+				//Or we just say that we need to have
+				// a player that needs to be included
 				event.respond("I'm sorry, you must include a person to be un/banned");
 				return;
+				
 			}
 			
+			//Someone could be trying to perform an un/mute
 			if ((argments[1].equalsIgnoreCase("mute")
 					|| argments[1].equalsIgnoreCase("unmute"))
+					
+					//So, once again, we ask if they have any sort
+					// of authority in this channel...
 					&& thisChan.isOp(event.getUser())) {
+				
 				CyniChat.printDebug("Muting...");
+				
+				//And whether they have provided a subject
 				if (argments[2] != null) {
 					
+					//Then we distinctify between a mute
 					if (argments[1].equalsIgnoreCase("mute")) {
-						IrcResponses.muteOutput(event.getUser(), event.getBot(), argments[2], thisChan.getName(), false);
+						
+						//We then mute said player
+						IrcResponses.muteOutput(event.getUser(), event.getBot(),
+								argments[2], thisChan.getName(), false);
+						
+					//And an unmute
 					} else {
-						IrcResponses.muteOutput(event.getUser(), event.getBot(), argments[2], thisChan.getName(), true);
+						
+						//Or unmute them...
+						IrcResponses.muteOutput(event.getUser(), event.getBot(),
+								argments[2], thisChan.getName(), true);
+						
 					}
+					
 					return;
 					
 				}
+				
+				//Or we tell them that we need a player to be
+				// muted/somesuch
 				event.respond("I'm sorry, you must include a person to be un/muted");
 				return;
+				
 			}
 			
+			//It could be the cheating command for
+			// talking an action through the bot
+			if ( argments[1].equalsIgnoreCase("talk")
+					
+					//As per asking... the player needs to be an
+					// op to get this talking message across
+					&& thisChan.isOp( event.getUser() )
+					
+					//Along with being in the master channel when
+					// they send this command.
+					&& thisChan.getName().equalsIgnoreCase( CyniChat.PBot.getIrcMasterChannel() ) ) {
+				
+				CyniChat.printDebug("Talking with " + argments.length + " args... ");
+				
+				//They have to have at least 4 argments...
+				// :? talk channel message
+				if (argments.length > 3) {
+					
+					CyniChat.printDebug("Talking...");
+					
+					//Then we try to talk
+					if ( !IrcResponses.talkOutput(
+							event.getBot(), argments[2],
+							stacker(argments, 3, argments.length)) )
+						
+						//And if that fails, say so
+						event.respond("Invalid statement. Please make sure that channel exits in the MC server.");
+					
+				}
+				
+				return;
+				
+			}
+			
+			//Someone could be asking for a bot restart
 			if (argments[1].equalsIgnoreCase("restart")
-					&& thisChan.isOp(event.getUser())) {
+					
+					//That someone must have authority...
+					&& thisChan.isOp(event.getUser())
+					
+					//In the master channel
+					&& thisChan.getName().equalsIgnoreCase( CyniChat.PBot.getIrcMasterChannel() ) ) {
+				
 				CyniChat.printDebug("Restarting...");
+				CyniChat.printDebug( String.format(
+						"%s is restarting CyniBot from channel %s",
+						event.getUser().getNick(),
+						thisChan.getName() ) );
+				
+				//Then we restart it.
 				CyniChat.PBot.restart();
 				return;
+				
 			}
 			
+			//Or, finally, they might be wanting to kill it
+			// dead.
 			if (argments[1].equalsIgnoreCase("kill")
-					&& thisChan.isOp(event.getUser())) {
+					
+					//Someone of this calibre must be an op
+					&& thisChan.isOp(event.getUser())
+					
+					//In the admin channel
+					&& thisChan.getName().equalsIgnoreCase( CyniChat.PBot.getIrcMasterChannel() ) ) {
+				
 				CyniChat.printDebug("Murdering...");
+				CyniChat.printDebug( String.format(
+						"%s is killing CyniBot from channel %s",
+						event.getUser().getNick(),
+						thisChan.getName() ) );
+				
+				//Alas, poor Yorick
 				CyniChat.PBot.stop();
 				return;
+				
 			}
 			
+			//Otherwise, it's garbage. Debug what the argument was
+			// and kill the method
 			CyniChat.printDebug("\"" + argments[1] + "\"");
 			
 			return;
+			
 		}
 		
+		//It's not a command, therefore it's a message that
+		// we have to pass on to the server. Let's get all 
+		// the details debugged into the console.
 		CyniChat.printDebug("Sender: " + event.getUser().getNick());
 		CyniChat.printDebug("Channel: " + event.getChannel().getName().toLowerCase());
 		CyniChat.printDebug("Message: " + event.getMessage());
 		
-		//Check if we are linked to this channel, and if so forward 
+		//Let's first get the channel name that we're in...
 		String ircChannelName = event.getChannel().getName().toLowerCase();
+		
+		//Then ask if we're linked to that channel
 		if (CyniChat.data.getLinkedChans().containsKey(ircChannelName)) {
-			Player[] online = Bukkit.getServer().getOnlinePlayers();
+			
+			//Player[] online = Bukkit.getServer().getOnlinePlayers();
+			
+			//Since we are... grab that channel
 			String channelName = CyniChat.data.getLinkedChans().get(ircChannelName);
 			
+			//And debug the details
 			CyniChat.printDebug("Match found: " + event.getChannel().getName() + " -> " + channelName);
 			
-			//Route message
-			ChatRouter.routeMessage(ChatRouter.EndpointType.IRC,event.getUser().getNick(), channelName, event.getMessage());
+			//Then send the message onto the main listener that's hanging
+			// around somewhere or other.
+			ChatRouter.routeMessage(ChatRouter.EndpointType.IRC,
+					event.getUser().getNick(), channelName, event.getMessage());
+			
 		}
+		
 	}
 	
+	/**
+	 * It could be a private message from someone or another.
+	 * So let's greet them warmly and give them a fuckton of 
+	 * commands to see and play with while they're at it.
+	 * 
+	 * @param event : This is the event that we're listening
+	 *  to with rapt attention
+	 */
 	@Override
 	public void onPrivateMessage(PrivateMessageEvent event) {
+		
+		//Tell the nice console what's happening
 		CyniChat.printDebug("Private message called!");
+		
+		//Then ask if someone is trying to do a command
 		if (event.getMessage().startsWith(":?")) {
+			
+			//And get all the substrings from it
 			String[] argments = event.getMessage().split(" ");
+			
+			//Tell the console that someone is doing
+			// something in private
 			CyniChat.printDebug(":? called by " + event.getUser().getNick());
 			
-			if (argments[1].equalsIgnoreCase("talk")) {
-				CyniChat.printDebug("Talking with " + argments.length + " args... ");
-				if (argments.length > 3) {
-					
-					CyniChat.printDebug("Talking...");
-					if (IrcResponses.talkOutput(event.getBot(), argments[2], stacker(argments, 3, argments.length)) == false) {
-						event.respond("Invalid statement. Please make sure that channel exits in the MC server.");
-					}
-				}
-				
-				return;
-			}
-			
+			//And then show them all the help they
+			// could ever need
 			IrcResponses.helpOutput(event.getBot(), event.getUser());
+			
 		}
+		
 	}
 	
+	/**
+	 * Finally, it could be a stacked sentence request from
+	 * the talk command. Let's get an array of words with a
+	 * start and an end position, then make the sentence.
+	 * 
+	 * @param args : This is the array of words we've been
+	 *  given to stack into a sentence
+	 * @param start : This is the starting word that is the
+	 *  first word in the sentence
+	 * @param end : This is the end position of the last
+	 *  word in the sentence
+	 * @return the final sentence, from start to end.
+	 */
 	public String stacker(String[] args, int start, int end) {
 		
+		//First of all, let's make a few strings to 
+		// hold all the information that we're going
+		// to generate from this.
 		String finalString = "";
 		String connector = "";
 		
+		//Then, from start to finish...
 		for (int i = start; i < end; i++) {
+			
+			//Add the word to the final sentence
 			finalString += connector + args[i];
+			
+			//And update the connector
 			connector = " ";
+			
 		}
 		
+		//Then let's return the final stacked sentence
 		return finalString;
+		
 	}
+	
 }
