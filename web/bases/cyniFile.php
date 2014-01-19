43532e2b2e92813f9c52a35c08eaf93497a90016
J4Numbers <j4numbers@gmail.com>
2014-01-19 06:09:07 +0000

    Did a lot of groundwork in dragging a few things from a forum thing into this that should allow for me to speed up the install process greatly. Also added in more \n's because woot!

--- web/bases/cyniFile.php
+++ web/bases/cyniFile.php
@@ -1,51 +1,59 @@
 <?php
 
-require_once 'I:\\github\\cynichat\\web\\config\\config.php';
+require_once "$home_dir/config/config.php";
 
-class cyniFile {
+class cyniFile implements database {
 
-private $players;
-private $channels;
+	private $players;
+	private $channels;
 
-public function objectToArray($d) {
-	if (is_object($d)) {
-		// Gets the properties of the given object
-		// with get_object_vars function
-		$d = get_object_vars($d);
+	public function objectToArray($d) {
+		if (is_object($d)) {
+			// Gets the properties of the given object
+			// with get_object_vars function
+			$d = get_object_vars($d);
+		}
+
+		// Return array
+		return $d;
 	}
 
-	// Return array
-	return $d;
-}
+	public function __construct() {
 
-public function __construct() {
+		$players = json_decode( file_get_contents( DATA_PATH . "players.json" , true ) );
+		$channels = json_decode( file_get_contents( DATA_PATH . "channels.json" , true ) );
 
-	$players = json_decode( file_get_contents( DATA_PATH . "players.json" , true ) );
-	$channels = json_decode( file_get_contents( DATA_PATH . "channels.json" , true ) );
-	
-	$this->players = $this->objectToArray( $players );
-	$this->channels = $this->objectToArray( $channels );
-	
-	//var_dump( $this->players );
-	//var_dump( $this->channels );
+		$this->players = $this->objectToArray( $players );
+		$this->channels = $this->objectToArray( $channels );
 
-}
+		//var_dump( $this->players );
+		//var_dump( $this->channels );
 
-public function getAllPlayers() {
-	return array_keys( $this->players );
-}
+	}
 
-public function getAllChannels() {
-	return array_keys( $this->channels );
-}
+	public function getAllPlayers() {
+		return array_keys( $this->players );
+	}
 
-public function getPlayer( $player ) {
-	return $this->objectToArray( $this->players[$player] );
-}
+	public function getAllChannels() {
+		return array_keys( $this->channels );
+	}
 
-public function getChannel( $channel ) {
-	return $this->objectToArray( $this->channels[$channel] );
-}
+	public function getOnePlayer( $player ) {
+		return $this->objectToArray( $this->players[$player] );
+	}
+
+	public function getOneChannel( $channel ) {
+		return $this->objectToArray( $this->channels[$channel] );
+	}
+
+	public function setOnePlayer( $player, $options ) {
+
+	}
+
+	public function setOneChannel( $channel, $options ) {
+
+	}
 
 }
 
